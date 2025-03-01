87009671b8080bead037a470b12d8f59847dbec2
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2025-02-28 23:23:23 -0600

    fixup libbcsv toml- update example c++ file

--- test.cc
+++ test.cc
@@ -2,6 +2,6 @@
 
 int main() {
     std::vector<uint8_t> buffer{};
-    libbcsv::csv_to_bcsv("BlueCoinIDRangeTable.csv", 0, buffer);
+    libbcsv::csv_to_bcsv("BlueCoinIDRangeTable.csv", 0);
     printf("%llu\n", buffer.size());
 }
