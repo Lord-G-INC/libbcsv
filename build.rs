518c1175f9e07bbb01754171761460405008fa59
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2025-02-28 23:44:23 -0600

    somehow. unix.

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     #[cfg(feature = "cxx")] {
-        println!("cargo::rerun-if-changed=src/cxx_exports.rs");
+        println!("cargo::rerun-if-changed=src\\cxx_exports.rs");
         cxx_build::bridge("src\\cxx_exports.rs")
         .compile("libbcsv");
     }
