0413631dea258a0dbf0c55974ea217fdd2ca950a
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2025-03-01 00:31:14 -0600

    finally fix doc building

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     #[cfg(feature = "cxx")] {
-        println!("cargo::rerun-if-changed=src\\cxx_exports.rs");
-        cxx_build::bridge("src\\cxx_exports.rs")
+        println!("cargo::rerun-if-changed=src/cxx_exports.rs");
+        cxx_build::bridge("src/cxx_exports.rs")
         .compile("libbcsv");
     }
 }
